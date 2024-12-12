0e8330e35b189603f80d6af41d39e7847228a929
pam0245 <paulmoturi@my.unt.edu>
2024-12-12 11:43:32 -0600

    adding website features

---
+++ users/settings.py
@@ -0,0 +1,14 @@
+# settings.py
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Default context processors...
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, f'Welcome back, {username}!')
+            return redirect('home')  # Redirect to your home page or dashboard
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'users/login.html')
+
